0c6c1649516822f5f2e770b9ebabb56ef3ca7eca
Salman Rahman <salman.rahman515@gmail.com>
2020-11-14 01:51:09 -0500

    included the class headers to build - namespace single include didn't work yet

--- realtime_vehicle_monitoring_diagnostics.cc
+++ realtime_vehicle_monitoring_diagnostics.cc
@@ -1,15 +1,19 @@
 #include <cstdlib>
 #include <iostream>
 
-// #include "dataset_manager.h"
-//using namespace realtime_vehicle_monitoring_diagnostics;
+#include "dataset_manager.h"
+#include "scheduler.h"
+#include "thread_manager.h"
+#include "timer_manager.h"
+
+using namespace realtime_vehicle_monitoring_diagnostics;
 
 int main(int argc, char *argv[])
 {
-	// realtime_vehicle_monitoring_diagnostics::dataset_manager ds_manager_obj = realtime_vehicle_monitoring_diagnostics::dataset_manager();
-	// scheduler scheduler_obj = scheduler();
-	// thread_manager thread_manager_obj = thread_manager();
-	// timer_manager timer_manager_obj = timer_manager();
+	dataset_manager ds_manager_obj = dataset_manager();
+	scheduler scheduler_obj = scheduler();
+	thread_manager thread_manager_obj = thread_manager();
+	timer_manager timer_manager_obj = timer_manager();
 
 	return EXIT_SUCCESS;
 }
