9555535106d5c3516a0773d1cf6b3bf076d225d5
Salman Rahman <salman.rahman515@gmail.com>
2020-11-14 01:00:01 -0500

    created dataset class files

---
+++ dataset_manager.cpp
@@ -0,0 +1,21 @@
+/*
+ * dataset_manager.cpp
+ *
+ *  Created on: Nov 14, 2020
+ *      Author: Salman
+ */
+
+#include "dataset_manager.h"
+
+namespace realtime_vehicle_monitoring_diagnostics {
+
+dataset_manager::dataset_manager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+dataset_manager::~dataset_manager() {
+	// TODO Auto-generated destructor stub
+}
+
+}
---
+++ dataset_manager.h
@@ -0,0 +1,21 @@
+/*
+ * dataset_manager.h
+ *
+ *  Created on: Nov 14, 2020
+ *      Author: Salman
+ */
+
+#ifndef DATASET_MANAGER_H_
+#define DATASET_MANAGER_H_
+
+namespace realtime_vehicle_monitoring_diagnostics {
+
+class dataset_manager {
+public:
+	dataset_manager();
+	virtual ~dataset_manager();
+};
+
+}
+
+#endif /* DATASET_MANAGER_H_ */
