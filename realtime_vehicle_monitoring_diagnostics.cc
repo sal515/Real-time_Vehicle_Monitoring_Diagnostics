7bc576f840f87ed60853f310f5e10d9be33df52e
Salman Rahman <salman.rahman515@gmail.com>
2020-12-02 21:37:28 -0500

    Added taskname enum and next_value func

--- realtime_vehicle_monitoring_diagnostics.cc
+++ realtime_vehicle_monitoring_diagnostics.cc
@@ -8,22 +8,52 @@ char *getcwd(char *buf, size_t size);
 using namespace std;
 #include <iostream>
 
-int main() {
+enum Task_Name
+{
+	fuel_consumption,
+	engine_speed_rpm,
+	engine_coolant_temp,
+	current_gear,
+	transmission_oil_temp,
+	vehicle_speed,
+	acceleration_speed_longitudinal,
+	indication_break_switch
+}
 
-  int counter=0;
+/* Provides the value for the task identified by the parameter */
+int
+read_next_value(Task_Name task_name, unsigned time_now_ms)
+{
+	int next_value;
+	return next_value;
+
+	//eg.
+	/* 
+	Last time @t=0;
+	Next time this should return the value for the task at @t=1
+	 */
+}
+
+int main()
+{
+
+	int counter = 0;
 	std::ifstream file("data.csv");
-	if (file.is_open()) {
-	    std::string line;
-	    while (std::getline(file, line)) {
-	    	if (counter > 5)
-	    	{return 0; }
-	        // using printf() in all tests for consistency
-	        printf("%s", line.c_str());
-	        counter ++;
-	    }
-	    file.close();
+	if (file.is_open())
+	{
+		std::string line;
+		while (std::getline(file, line))
+		{
+			if (counter > 5)
+			{
+				return 0;
+			}
+			// using printf() in all tests for consistency
+			printf("%s", line.c_str());
+			counter++;
+		}
+		file.close();
 	}
 
-
-return 0;
+	return 0;
 }
