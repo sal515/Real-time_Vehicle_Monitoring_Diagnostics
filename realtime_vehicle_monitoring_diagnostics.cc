1ee31d7fabac34871c6cadd4c8188a37e905faf4
Salman Rahman <salman.rahman515@gmail.com>
2020-12-02 21:57:33 -0500

    Added task name structures, producer output struct and get_next_value func

--- realtime_vehicle_monitoring_diagnostics.cc
+++ realtime_vehicle_monitoring_diagnostics.cc
@@ -8,22 +8,66 @@ char *getcwd(char *buf, size_t size);
 using namespace std;
 #include <iostream>
 
-int main() {
+enum TASK_NAME
+{
+	FUEL_CONSUMPTION,
+	ENGINE_SPEED_RPM,
+	ENGINE_COOLANT_TEMP,
+	CURRENT_GEAR,
+	TRANSMISSION_OIL_TEMP,
+	VEHICLE_SPEED,
+	ACCELERATION_SPEED_LONGITUDINAL,
+	INDICATION_BREAK_SWITCH
+};
 
-  int counter=0;
+struct PRODUCER_VALUES
+{
+	int fuel_consumption;
+	int engine_speed_rpm;
+	int engine_coolant_temp;
+	int current_gear;
+	int transmission_oil_temp;
+	int vehicle_speed;
+	int acceleration_speed_longitudinal;
+	int indication_break_switch;
+};
+
+PRODUCER_VALUES producer_buffer;
+
+/* Provides the value for the task identified by the parameter */
+int read_next_value(int task_name, unsigned int time_now_ms)
+{
+	int next_value = 0;
+	producer_buffer.fuel_consumption = next_value;
+	return next_value;
+
+	//eg.
+	/*
+	Last time @t=0;
+	Next time this should return the value for the task at @t=1
+	 */
+}
+
+int main()
+{
+
+	int counter = 0;
 	std::ifstream file("data.csv");
-	if (file.is_open()) {
-	    std::string line;
-	    while (std::getline(file, line)) {
-	    	if (counter > 5)
-	    	{return 0; }
-	        // using printf() in all tests for consistency
-	        printf("%s", line.c_str());
-	        counter ++;
-	    }
-	    file.close();
+	if (file.is_open())
+	{
+		std::string line;
+		while (std::getline(file, line))
+		{
+			if (counter > 5)
+			{
+				return 0;
+			}
+			// using printf() in all tests for consistency
+			printf("%s", line.c_str());
+			counter++;
+		}
+		file.close();
 	}
 
-
-return 0;
+	return 0;
 }
