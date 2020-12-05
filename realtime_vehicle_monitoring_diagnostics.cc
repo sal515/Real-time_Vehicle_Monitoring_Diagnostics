1a334d604ba2bf1cdff4ef4cc1b1a90ea0d6ee10
Sherby <sherby93@hotmail.com>
2020-12-04 23:17:47 -0500

    fct done

--- realtime_vehicle_monitoring_diagnostics.cc
+++ realtime_vehicle_monitoring_diagnostics.cc
@@ -7,6 +7,10 @@ char *getcwd(char *buf, size_t size);
 #include <string>
 using namespace std;
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
 
 enum TASK_NAME
 {
@@ -22,24 +26,74 @@ enum TASK_NAME
 
 struct PRODUCER_VALUES
 {
-	int fuel_consumption;
-	int engine_speed_rpm;
-	int engine_coolant_temp;
-	int current_gear;
-	int transmission_oil_temp;
-	int vehicle_speed;
-	int acceleration_speed_longitudinal;
-	int indication_break_switch;
+	string fuel_consumption;
+	string engine_speed_rpm;
+	string engine_coolant_temp;
+	string current_gear;
+	string transmission_oil_temp;
+	string vehicle_speed;
+	string acceleration_speed_longitudinal;
+	string indication_break_switch;
 };
 
 PRODUCER_VALUES producer_buffer;
 
 /* Provides the value for the task identified by the parameter */
-int read_next_value(int task_name, unsigned int time_now_ms)
+string read_next_value(int task_name, string time_now_ms)
 {
-	int next_value = 0;
-	producer_buffer.fuel_consumption = next_value;
-	return next_value;
+	string next_value;
+	//producer_buffer.fuel_consumption = next_value;
+	string time = time_now_ms;
+	ifstream input_from_file("data.csv");
+	    string line;
+	    //row x =time
+	    while (getline(input_from_file, line)) {
+	        line += ",";
+	        stringstream ss(line);
+	        string word;
+
+	        //new vector each iteration
+	        vector<string> words;
+
+	     //column of row x (time)
+	        while (getline(ss, word, ',')){
+
+                //cout<<"word is: "<<word<<endl;
+                words.push_back(word);
+	        }
+                while(1){
+                	        	if(words[7]==time){
+                                cout<<"In the fct: "<<words[task_name]<<endl;
+                                next_value = words[task_name];
+                               // words.push_back(word);
+                                break;}else{break;}
+
+
+                }
+/*
+	        pour le begin = 0 to end = 1000 ms
+	        time == end / 1000 in seconds
+
+	       for (time/period_of_variable)
+	    	   oil temp = 268.8
+
+
+
+	        temps est ce qui est representer dans le file
+	        next period : begin=begin+1000; end=end+1000;
+
+	        //cout<<"new row coming up"<<endl;
+	       // cout<<"word[5] : "<<words[5]<<endl;
+	        //
+
+*/
+	           // while (getline(ss, word, ','))
+	             //              cout<<"word is: "<<word<<endl;
+	           	     //       words.push_back(word);
+
+	    }
+
+	     return next_value;
 
 	//eg.
 	/*
@@ -53,7 +107,7 @@ int main()
 
 //	int output = read_next_value(FUEL_CONSUMPTION, 2005);
 //	printf("%d", output);
-
+/*
 	int counter = 0;
 	std::ifstream file("data.csv");
 	if (file.is_open())
@@ -61,16 +115,27 @@ int main()
 		std::string line;
 		while (std::getline(file, line))
 		{
-			if (counter > 5)
+			if (counter > 6)
 			{
 				return 0;
 			}
 			// using printf() in all tests for consistency
 			printf("%s", line.c_str());
 			counter++;
+
+
 		}
 		file.close();
 	}
 
 	return 0;
 }
+*/
+string time = "5";
+
+	    producer_buffer.engine_speed_rpm = read_next_value(ENGINE_SPEED_RPM, time);
+
+	    cout<<"in main: "<<producer_buffer.engine_speed_rpm<<endl;
+
+	    return 0;
+	}
