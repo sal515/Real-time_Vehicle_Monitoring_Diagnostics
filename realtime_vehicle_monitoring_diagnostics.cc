dc12314a9a3659ffe614b64a790ae53dcb8ee86b
Salman Rahman <salman.rahman515@gmail.com>
2020-12-06 23:12:37 -0500

    Completed the converter function task name to col name

--- realtime_vehicle_monitoring_diagnostics.cc
+++ realtime_vehicle_monitoring_diagnostics.cc
@@ -83,17 +83,44 @@ struct PRODUCER_VALUES
 	std::string indication_break_switch;
 };
 
-PRODUCER_VALUES producer_buffer;
+struct PRODUCER_VALUES producer_buffer;
 pthread_mutex_t data_mutex;
 std::vector<std::string> readValues;
 
-int string_to_enum_converter(char *task_name)
+int producer_string_to_enum_converter(char *task_name)
 {
-	// if(task_name == "fuel_consumption"){
-	// 	return FUEL_CONSUMPTION;
-	// }
-	// else if()
-	/* TODO: Implement  */
+	if (!std::strcmp("fuel_consumption", task_name))
+	{
+		return FUEL_CONSUMPTION;
+	}
+	else if (!std::strcmp("engine_speed_rpm", task_name))
+	{
+		return ENGINE_SPEED_RPM;
+	}
+	else if (!std::strcmp("engine_coolant_temp", task_name))
+	{
+		return ENGINE_COOLANT_TEMP;
+	}
+	else if (!std::strcmp("current_gear", task_name))
+	{
+		return CURRENT_GEAR;
+	}
+	else if (!std::strcmp("transmission_oil_temp", task_name))
+	{
+		return TRANSMISSION_OIL_TEMP;
+	}
+	else if (!std::strcmp("vehicle_speed", task_name))
+	{
+		return VEHICLE_SPEED;
+	}
+	else if (!std::strcmp("acceleration_speed_longitudinal", task_name))
+	{
+		return ACCELERATION_SPEED_LONGITUDINAL;
+	}
+	else if (!std::strcmp("indication_break_switch", task_name))
+	{
+		return INDICATION_BREAK_SWITCH;
+	}
 }
 
 /* Provides the value for the task identified by the parameter */
@@ -133,20 +160,20 @@ std::string read_next_value(int task_name, std::string time_now_ms)
 
 int main(int argc, char *argv[])
 {
-	/* For tests */
-	// 		// test code here
-	// return 0;
-	/* CLEAN: Test */
-    float timer;
-	std::string time;
-	timer = 1005/1000;
-			std::stringstream ss;
-					ss <<timer;
-					time= ss.str();
-	producer_buffer.engine_speed_rpm = read_next_value(ENGINE_SPEED_RPM, time);
-	printf("in main: %s\n", producer_buffer.engine_speed_rpm.c_str());
+	// /* For tests */
+	// // 		// test code here
+	// // return 0;
+	// /* CLEAN: Test */
+	// float timer;
+	// std::string time;
+	// timer = 1005/1000;
+	// 		std::stringstream ss;
+	// 				ss <<timer;
+	// 				time= ss.str();
+	// producer_buffer.engine_speed_rpm = read_next_value(ENGINE_SPEED_RPM, time);
+	// printf("in main: %s\n", producer_buffer.engine_speed_rpm.c_str());
 
-	return 0;
+	// return 0;
 
 	pthread_mutex_init(&data_mutex, NULL);
 	build_periodic_tasks_list(&scheduler);
@@ -182,13 +209,18 @@ void *consumer(void *args)
 
 	thread->block();
 	pthread_mutex_lock(&data_mutex);
-	printf("Consumer: Data Processed\n");
+
+	Logger::log_thread_details(thread, "Details of the thread - waking up:");
+	// -- critical section --
+
 	// read from producer_buffer and print out using printf
 	std::string value;
 	value = readValues[0];
-	printf("task name : %s\n", value.c_str() );
+	printf("task name : %s\n", value.c_str());
 	readValues.clear();
 	// read_next_value(task_name, time_now_ms);
+
+	// -- critical section --
 	pthread_mutex_unlock(&data_mutex);
 	thread->unblock();
 
@@ -209,32 +241,34 @@ void *producer(void *args)
 	Logger::log_thread_details(thread, "Details of the thread - waking up:");
 	// -- critical section --
 
-	// -- critical section --
-	printf("Producer: Data Processed\n");
-    int timer = 0;
-    std::string string_time;//time we pass to the function
-    std::string to_string( int value );
+	int timer = 0;
+	std::string string_time; //time we pass to the function
+	// std::string to_string(int value);
 
 	// write to producer_buffer from file
 	/*take timer storage and take the floor */
-	if(timer_storage < 1){
+	if (timer_storage < 1)
+	{
 		timer = 1;
-
 		std::stringstream ss;
-		ss <<timer;
-		string_time= ss.str();}
-	else{//floor of timer storage
-		timer = timer_storage/1000;
+		ss << timer;
+		string_time = ss.str();
+	}
+	else
+	{ //floor of timer storage
+		timer = timer_storage / 1000;
 		std::stringstream ss;
-				ss <<timer;
-				string_time= ss.str();
+		ss << timer;
+		string_time = ss.str();
 	}
 
 	std::string value;
-     value = read_next_value(string_to_enum_converter(task_name), string_time);
-    readValues.push_back(value);
+	value = read_next_value(producer_string_to_enum_converter(task_name), string_time);
+	readValues.push_back(value);
 
 	// read_next_value(task_name, time_now_ms);
+
+	// -- critical section --
 	pthread_mutex_unlock(&data_mutex);
 	thread->unblock();
 
